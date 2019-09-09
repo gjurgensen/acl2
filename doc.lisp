3c38a9221d2f92675deb08f81553f31c30bffadb
Matt Kaufmann <kaufmann@cs.utexas.edu>
2019-09-09 09:10:21 -0500

    Synched doc.lisp

--- doc.lisp
+++ doc.lisp
@@ -83015,9 +83015,9 @@ Heuristic and Efficiency Improvements
   [maybe-flush-and-compress1].
 
   Improved the speed of [theory] updates by avoiding repeated length
-  computations.  As a result, we have seen about a 5% reduction in
-  the time for executing the form, (include-book \"centaur/sv/top\"
-  :dir :system).
+  computations.  As a result, we have seen about a 5% time reduction
+  on MacOS, and between 3% and 4% on Linux, in the time for executing
+  the form, (include-book \"centaur/sv/top\" :dir :system).
 
 
 Bug Fixes
