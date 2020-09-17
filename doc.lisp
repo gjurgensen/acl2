225b4cd2d1b4026b832cb77b24a190a2fa8a92db
Matt Kaufmann <kaufmann@cs.utexas.edu>
2020-09-16 19:06:43 -0500

    Synched doc.lisp etc.
    Note: Regressions that passed just before synching doc.lisp include a
    usual "everything" regression using CCL as well as a normal GCL
    regression.

--- doc.lisp
+++ doc.lisp
@@ -85732,6 +85732,15 @@ Bug Fixes
     * [Defun-sk] provided no direct way to allow formals to be ignored.
       Now, ignorable [declaration]s are permitted.
 
+  For Lisps that do not compile on the fly (that is, Lisps other than
+  CCL and SBCL), evaluation of :comp t effectively unmemoized
+  functions that were not already compiled.  This has been fixed.
+
+  When a [stobj] hash-table's ``init'' function was called with size 0,
+  an error could occur for host Lisp GCL, complaining that size 0 is
+  not allowed for hash-tables.  This has been fixed by using size 1
+  instead of 0 in this case.
+
 
 Changes at the System Level
 
