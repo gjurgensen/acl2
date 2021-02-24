46011ac1846f164f7e6ca6e179e6eb25e81f0d90
Matt Kaufmann <kaufmann@cs.utexas.edu>
2021-02-23 20:54:17 -0600

    Tweaked :doc rewrite-quoted-constant.
    I've updated :doc rewrite-quoted-constant to clarify that the rewriter
    does not explore inside constants when using :rewrite-quoted-constant
    rules, as per discussions with Mihir and J.

--- books/system/doc/acl2-doc.lisp
+++ books/system/doc/acl2-doc.lisp
@@ -101029,9 +101029,10 @@ arithmetic) for libraries of @(see books) for arithmetic reasoning.</p>")
   explore them down to the tips would be prohibitively expensive.</p>
 
   <p>However, form @('[2]') rules, of the general form @('(implies
-  hyps (equiv (fn var) var))'), allow you to do a root-and-branch exploration
-  of every quoted constant occuring in a given @('equiv') context and compute
-  the replacement constant with the normalizer.</p>
+  hyps (equiv (fn var) var))'), allow the normalizer to be applied to every
+  quoted constant occuring in a suitable @('equiv') context.  The normalizer
+  can then do a root-and-branch exploration of the constant to compute its
+  replacement.</p>
 
   <p>For example, the @('rewrite-quoted-constants-examples') book cited above
   defines the normalizer @('(drop-dups-and-sort var)') to coerce its argument
